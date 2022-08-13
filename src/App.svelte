79980822ece33d99ea82ad5b4aca9d7b3b788173
Yuta-31 <pineapple.gt@gmail.com>
2022-08-13 19:24:50 +0900

    change title

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,6 @@
-<script lang="ts">
-  import CounterCard from "./components/CounterCard.svelte";
-
-  let id: number = 1;
-  type Counter = {
-    id: number;
-    title: string;
-    count: number;
-  };
-
-  let counters: Counter[] = [{ id: 0, title: "new", count: 0 }];
-  const addNewCounter = (title: string, count: number) => {
-    counters = [...counters, { id, title, count }];
-    id += 1;
-  };
-  const removeCounter = (id: number) => {
-    counters = counters.filter((counter) => counter.id !== id);
-  };
-  $: sumOfCount = counters.reduce(
-    (total: number, counter: Counter): number => total + counter.count,
-    0
-  );
-  $: lastIdx = counters.reduce((lastIdx: number, counter: Counter): number => {
-    if (lastIdx < counter.id) {
-      return counter.id;
-    } else {
-      return lastIdx;
-    }
-  }, 0);
-  $: console.log(counters, lastIdx);
-</script>
+<svelte:head>
+	<title>Multiple Counter</title>
+</svelte:head>
 
 <main>
   <center>
@@ -60,6 +32,38 @@
     </div>
   </center>
 </main>
+
+<script lang="ts">
+	import CounterCard from "./components/CounterCard.svelte";
+  
+	let id: number = 1;
+	type Counter = {
+	  id: number;
+	  title: string;
+	  count: number;
+	};
+  
+	let counters: Counter[] = [{ id: 0, title: "new", count: 0 }];
+	const addNewCounter = (title: string, count: number) => {
+	  counters = [...counters, { id, title, count }];
+	  id += 1;
+	};
+	const removeCounter = (id: number) => {
+	  counters = counters.filter((counter) => counter.id !== id);
+	};
+	$: sumOfCount = counters.reduce(
+	  (total: number, counter: Counter): number => total + counter.count,
+	  0
+	);
+	$: lastIdx = counters.reduce((lastIdx: number, counter: Counter): number => {
+	  if (lastIdx < counter.id) {
+		return counter.id;
+	  } else {
+		return lastIdx;
+	  }
+	}, 0);
+	$: console.log(counters, lastIdx);
+  </script>
 
 <style>
   .title {
