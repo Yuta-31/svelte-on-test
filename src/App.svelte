7a41abde9375223ab8305a2eee8b01f38db4f958
Yuta-31 <pineapple.gt@gmail.com>
2022-08-13 19:06:40 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import CounterCard from "./components/CounterCard.svelte";
+
+  let id: number = 1;
+  type Counter = {
+    id: number;
+    title: string;
+    count: number;
+  };
+
+  let counters: Counter[] = [{ id: 0, title: "new", count: 0 }];
+  const addNewCounter = (title: string, count: number) => {
+    counters = [...counters, { id, title, count }];
+    id += 1;
+  };
+  const removeCounter = (id: number) => {
+    counters = counters.filter((counter) => counter.id !== id);
+  };
+  $: sumOfCount = counters.reduce(
+    (total: number, counter: Counter): number => total + counter.count,
+    0
+  );
+  $: lastIdx = counters.reduce((lastIdx: number, counter: Counter): number => {
+    if (lastIdx < counter.id) {
+      return counter.id;
+    } else {
+      return lastIdx;
+    }
+  }, 0);
+  $: console.log(counters, lastIdx);
+</script>
+
+<main>
+  <center>
+    <div class="title">Multiple Counter</div>
+    {#each counters as counter (counter.id)}
+      <CounterCard
+        bind:title={counter.title}
+        bind:count={counter.count}
+        remove={() => {
+          removeCounter(counter.id);
+        }}
+      />
+    {/each}
+    <div on:click={() => addNewCounter("new", 0)} class="new-counter-button">
+      new counter
+    </div>
+    <div>
+      title list:
+      {#each counters as counter (counter.id)}
+        {#if lastIdx == counter.id}
+          {counter.title}&nbsp;
+        {:else}
+          {counter.title},&nbsp;
+        {/if}
+      {/each}
+    </div>
+    <div>
+      sum of count: {sumOfCount}
+    </div>
+  </center>
+</main>
+
+<style>
+  .title {
+    font-weight: 400;
+    font-size: 4rem;
+    margin-bottom: 10px;
+  }
+  .new-counter-button {
+    background-color: #68d391;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    width: 384px;
+    height: 24px;
+    cursor: pointer;
+    line-height: 24px;
+  }
+</style>
---
+++ src/components/CounterCard.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  export let count: number = 0;
+  export let title: string = "new";
+  export let remove: () => void;
+
+  const increment = () => {
+    count += 1;
+  };
+  const decrement = () => {
+    if (count > 0) count -= 1;
+  };
+  const setZero = () => {
+    count = 0;
+  };
+</script>
+
+<div class="counter">
+  <input type="text" name="title" bind:value={title} />
+  <span>{count}</span>
+  <div class="button inc" on:click={increment}>+</div>
+  <div class="button dec" on:click={decrement}>-</div>
+  <div class="button zero" on:click={setZero}>0</div>
+  <div class="button x" on:click={remove}>x</div>
+</div>
+
+<style>
+  .counter {
+    width: 384px;
+    height: 43px;
+    margin: 0 0 0.5rem 0;
+    border-radius: 3px;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.24);
+    background-color: #f7fafc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .counter input {
+    border: none;
+    width: 128px;
+    height: 24px;
+    margin-top: 10px;
+    margin-left: 16px;
+    margin-right: 26px;
+    color: gray;
+  }
+
+  .counter span {
+    width: 44px;
+    height: 27px;
+    margin-right: 35px;
+    padding-top: 7px;
+    font-weight: bold;
+    font-size: 1.125rem;
+  }
+
+  .counter .button {
+    width: 35px;
+    height: 35px;
+    border: none;
+    color: white;
+    font-size: 1.125rem;
+    cursor: pointer;
+    line-height: 35px;
+  }
+  .counter .inc {
+    background-color: #f56565;
+    border-radius: 5px 0 0 5px;
+  }
+  .counter .dec {
+    background-color: #4299e1;
+    border-radius: 0;
+  }
+  .counter .zero {
+    background-color: #ecc94b;
+    border-radius: 0 5px 5px 0;
+  }
+  .counter .x {
+    background-color: none;
+    color: gray;
+  }
+</style>
